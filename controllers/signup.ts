177fb90e113d1fec6c15eb185d09a5db26a2c8cc
Alvin Hart <105436283+greenzombie123@users.noreply.github.com>
2025-11-16 17:48:50 +0900

    Create validation for username and password

--- controllers/signup.ts
+++ controllers/signup.ts
@@ -1,5 +1,25 @@
 import type { Request, Response, NextFunction } from "express";
+import { body, validationResult,matchedData } from "express-validator";
 
-export const getSignUpPage = (req:Request, res:Response)=>{
-    res.render('signupPage')    
+export const getSignUpPage = (req: Request, res: Response) => {
+  res.render("signupPage");
+};
+
+export const registerUser = (req:Request, res:Response, next:NextFunction)=>{
+    res.send(req.body)
 }
+
+// Call this when user is trying to register. Checks if all fields are filled or not
+export const checkSignUpForm = () =>
+  body(["username", "password"]).notEmpty().escape();
+
+// Called after checkSignUpForm
+export const validateSignUpForm = (req: Request, res: Response,next: NextFunction) => {
+
+  // Get validation results
+  const result = validationResult(req);
+  
+  // Check if there are no validation errors
+  if (result.isEmpty()) next();
+  else res.send({ errors: result.array() });
+};
--- routes/signup.ts
+++ routes/signup.ts
@@ -1,11 +1,9 @@
 import { Router } from "express";
-import { getSignUpPage } from "../controllers/signup.ts";
+import { checkSignUpForm, getSignUpPage, registerUser, validateSignUpForm } from "../controllers/signup.ts";
 
 const signupRouter = Router();
 
 signupRouter.get("/", getSignUpPage)
-// signupRouter.get("/", getSignUpPage)
-
-// signupRouter.post("/")
+signupRouter.post("/", checkSignUpForm(), validateSignUpForm, registerUser)
 
 export default signupRouter
