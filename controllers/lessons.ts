4ecc2f5ec92a325277ca3c078ef7b0197099c3ad
Alvin Hart <105436283+greenzombie123@users.noreply.github.com>
2025-11-28 08:22:56 +0900

    Can get lessons from database

--- controllers/lessons.ts
+++ controllers/lessons.ts
@@ -1,8 +1,45 @@
 import type { Request, Response, NextFunction } from "express";
+import { pool } from "../pool.ts";
 
-export const getDashBoard = (req:Request, res:Response, next:NextFunction)=>{
-    if(!req.user) return res.redirect('/')
-    const {username, userType} = req.user
-    res.locals = {username, userType}
-    res.render('dashboard')
+type LessonData = {
+    name:string,
+    id:number
 }
+
+export const getDashBoard = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (!req.user) return res.redirect("/");
+  const { username, userType } = req.user;
+  const lessonsData = await getLessons(userType, username)
+
+  res.locals = { username, userType, lessonsData };
+  res.render("dashboard");
+};
+
+
+const getLessons = async (userType: string, userName: string) => {
+    const queryString = getQueryString(userType)
+
+    console.log(queryString, userName)
+    const {rows} = await pool.query<LessonData>(queryString, [userName])
+
+    return rows
+};
+
+const getQueryString = (userType: string) => {
+  if (userType === "student") {
+    return `SELECT lessons.name, lessons.id FROM lessons
+            INNER JOIN teachers
+            ON lessons.teacherid = teachers.id
+            INNER JOIN teachers_students
+            ON teachers.username = teachers_students.teacherusername
+            WHERE teachers_students.studentusername = $1`;
+  } else
+    return `SELECT lessons.name, lessons.id FROM lessons
+            INNER JOIN teachers
+            ON lessons.teacherid = teachers.id
+            WHERE teachers.username = $1`;
+};
